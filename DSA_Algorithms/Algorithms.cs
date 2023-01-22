940c13de1858e4d5235f272e19ca117b49306808
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-22 22:43:57 +0530

    [SUNIL]:Add Code For InsertionSort

--- DSA_Algorithms/Algorithms.cs
+++ DSA_Algorithms/Algorithms.cs
@@ -49,5 +49,30 @@ namespace DSA_Algorithms
                 Console.WriteLine("{0},", arr[i]);
             }
         }
+        public void InsertionSort(int[] arr)
+        {
+            
+            int size = arr.Length;
+            for (int i = 1; i < size; i++)
+            {
+                int k = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > k)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = k;
+            }
+        }
+        public void Display(int[] arr)
+        {
+            Console.WriteLine("The Sorted array is : ");
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
     }
 }
--- DSA_Algorithms/Program.cs
+++ DSA_Algorithms/Program.cs
@@ -6,7 +6,12 @@
         {
             Algorithms run = new Algorithms();
             //run.Anagram();
-            run.BubbleSort();
+            //run.BubbleSort();
+            //InsertionSort insertionSort = new InsertionSort();
+
+            int[] arr = { 60, 20, 4, 35, 10, 15, 5 };
+            run.InsertionSort(arr);
+            run.Display(arr);
         }
     }
 }
