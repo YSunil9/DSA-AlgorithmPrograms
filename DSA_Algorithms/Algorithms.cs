2aea376b684eb1fb901da764cc7a6765cea4659b
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-23 20:40:32 +0530

    [SUNIL]:Add Code For Check Permutations

--- DSA_Algorithms/Algorithms.cs
+++ DSA_Algorithms/Algorithms.cs
@@ -8,176 +8,30 @@ namespace DSA_Algorithms
 {
     internal class Algorithms
     {
-        public void Anagram()
+        public void Permutations(string input, int left, int right)
         {
-            Console.WriteLine("Enter first String");
-            string value1 = Console.ReadLine();
-            Console.WriteLine("Enter second String");
-            string value2 = Console.ReadLine();
-            char[] firstValue = value1.ToLower().ToCharArray();
-            char[] secondValue = value2.ToLower().ToCharArray();
-            string string1 = new string(firstValue);
-            string string2 = new string(secondValue);
-
-            if (string1 == string2)
+            if (left == right)
             {
-                Console.WriteLine("Strings are Anagrams");
+                Console.WriteLine(input);
             }
             else
             {
-                Console.WriteLine("Strings are not Anagrams");
-            }
-        }
-        public void BubbleSort()
-        {
-            int[] arr = new int[6] { 2, 8, 9, 0, 6, 7 };
-            int temp = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr.Length; j++)
+                for (int i = left; i <= right; i++)
                 {
-                    if (arr[i] < arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
+                    input = ChangePosition(input, left, i);
+                    Permutations(input, left + 1, right);
+                    input = ChangePosition(input, left, i);
                 }
             }
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine("{0},", arr[i]);
-            }
         }
-        public void InsertionSort(int[] arr)
+        public string ChangePosition(string input, int left, int right)
         {
-
-            int size = arr.Length;
-            for (int i = 1; i < size; i++)
-            {
-                int k = arr[i];
-                int j = i - 1;
-                while (j >= 0 && arr[j] > k)
-                {
-                    arr[j + 1] = arr[j];
-                    j = j - 1;
-                }
-                arr[j + 1] = k;
-            }
-        }
-        public void Display(int[] arr)
-        {
-            Console.WriteLine("The Sorted array is : ");
-            int n = arr.Length;
-            for (int i = 0; i < n; ++i)
-            {
-                Console.Write(arr[i] + " ");
-            }
-        }
-        public void Binary(string input)
-        {
-            string[] text = System.IO.File.ReadAllLines(@"D:\Bridgelab\repos\DSA_Algorithms\DSA_Algorithms\BinarySearch.txt");
-            for (int i = 0; i < text.Length; i++)
-            {
-                int val = string.Compare(input, text[i]);
-                if (val == 0)
-                {
-                    Console.WriteLine(text[i]);
-                    Console.WriteLine("Entered word present on " + i + " index");
-                    return;
-                }
-            }
-            Console.WriteLine("Word not present");
-            return;
-        }
-        public void PrimeNumber()
-        {
-            Console.WriteLine("Enter the Number : ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Prime Numbers between 0 and 1000 are :");
-            for (int j = 2; j <= number; j++)
-            {
-                int count = 0;
-                for (int i = 2; i <= j / 2; i++)
-                {
-                    if ((j % i) == 0)
-                    {
-                        count++;
-                        break;
-                    }
-                }
-                if (count == 0)
-                    Console.Write(j + " ");
-            }
-        }
-        //MergeSort
-        public void Merge(int[] array, int left, int middle, int right)
-        {
-            int n1 = middle - left + 1;
-            int n2 = right - middle;
-
-            int[] temp1 = new int[n1];
-            int[] temp2 = new int[n2];
-            int i, j;
-
-            for (i = 0; i < n1; i++)
-            {
-                temp1[i] = array[left + i];
-            }
-            for (j = 0; j < n2; j++)
-            {
-                temp2[j] = array[middle + 1 + j];
-            }
-            int k = left;
-            i = 0;
-            j = 0;
-
-            while (i < n1 && j < n2)
-            {
-                if (temp1[i] <= temp2[j])
-                {
-                    array[k] = temp1[i];
-                    i++;
-                }
-                else
-                {
-                    array[k] = temp2[j];
-                    j++;
-                }
-                k++;
-            }
-            while (i < n1)
-            {
-                array[k] = temp1[i];
-                i++;
-                k++;
-            }
-            while (j < n2)
-            {
-                array[k] = temp2[j];
-                j++;
-                k++;
-            }
-        }
-        public void sort(int[] array, int left, int right)
-        {
-            if (left < right)
-            {
-                int middle = left + (right - left) / 2;
-
-                sort(array, left, middle);
-                sort(array, 1 + middle, right);
-
-                Merge(array, left, middle, right);
-            }
-        }
-        public void Print(int[] array)
-        {
-            int length = array.Length;
-            for (int i = 0; i < length; i++)
-            {
-                Console.Write(array[i] + " ");
-            }
+            char[] tempString = input.ToCharArray();
+            char temp = tempString[left];
+            tempString[left] = tempString[right];
+            tempString[right] = temp;
+            string swapstring = new string(tempString);
+            return swapstring;
         }
     }
 }
--- DSA_Algorithms/Program.cs
+++ DSA_Algorithms/Program.cs
@@ -7,12 +7,10 @@ namespace DSA_Algorithms
         static void Main(string[] args)
         {
             Algorithms run = new Algorithms();
-            int[] array = { 30, 20, 50, 70, 120, 180, 100, 40 };
-            Console.WriteLine("Before Sorting:");
-            run.Print(array);
-            Console.WriteLine("\nAfter Sorting");
-            run.sort(array, 0, array.Length - 1);
-            run.Print(array);
+                Console.WriteLine("Enter string");
+                string input = Console.ReadLine();
+                Console.WriteLine("Permutations are");
+                run.Permutations(input, 0, input.Length - 1);          
         }
     }
 }
